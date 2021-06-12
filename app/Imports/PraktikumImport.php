a2fdf6bcef308669ce6c78833f6899446a84dab4
GusArjuna <taufanmaruf26@gmail.com>
2021-06-12 17:48:01 +0700

    completting

--- app/Imports/PraktikumImport.php
+++ app/Imports/PraktikumImport.php
@@ -2,20 +2,23 @@
 
 namespace App\Imports;
 
-use App\User;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Concerns\ToModel;
 
 class PraktikumImport implements ToModel
 {
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
         return new User([
-            //
+            'name'     => $row[0],
+            'email'    => $row[1],
+            'password' => Hash::make($row[2]),
         ]);
     }
 }
